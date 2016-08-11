be5c4606e9451ca846927fdc955a52edc315d96a
contact@alexwen.me <contact@alexwen.me>
2016-08-11 03:12:26 -0500

    Add contact page and contact form.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
         
   root 'welcome#index'
+
+  get 'contact' => 'contact#index'
+
   mount Spree::Core::Engine, :at => '/store'
 end
