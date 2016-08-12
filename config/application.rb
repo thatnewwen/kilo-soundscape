45f1e598397e1526b9f2b0e4413ddc2237bc4991
Jonathan Kaplan <jbkaplan@outlook.com>
2016-08-11 22:36:21 -0500

    Upate application.rb file for ENV variables

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Set ENV variables
+if File.exists?(File.expand_path('../application.yml', __FILE__))
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] = value.to_s unless value.kind_of? Hash
+  end
+end
+
 module KiloSoundscape
   class Application < Rails::Application
     
