594b265b915db4cc66d0c34cfd52eed8c6b60895
contact@alexwen.me <contact@alexwen.me>
2016-10-10 12:24:37 -0500

    Ignore email fails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
